1b5a3a28ef53a0f7c668c7ae1ce00dae7644a783
Pritam <physics.pritam@gmail.com>
2018-02-01 00:46:55 +0530

    Argumental i/p o/p files

--- python_parsing.py
+++ python_parsing.py
@@ -3,68 +3,35 @@ import os
 import re
 import sys
 
-os.chdir('/home/pk/Desktop/Programming/Python/parsing')
+print ('Number of arguments:', len(sys.argv), 'arguments.')
 
-# print(os.getcwd())
-# for f in os.listdir():
-#     print(f)
-
-
-#
-f = open('wmc_PR.log', 'r')
+f = open(sys.argv[1], 'r')
+f_out = open(sys.argv[2], 'w')
 f_contents = f.readline()  # reads single line
-# print(f_contents, end="")
-#
 
-#------ Read in a chunk --------
-
-# size_to_read = 10
-# while len(f_contents) > 0:
-#     print(f_contents, end='')
-#     f_contents = f.read(size_to_read)
-
-# look for pattern
 i = 1
 print("List of workflows: ")
-
-k = ["DoubleEG", "SinglePhoton", "JetHT", "DoubleMuon", "SingleMuon", "HLTPhysics", "MET", "ZeroBias"]
 
 arr = []
 
 for line in f:
     if re.match("Injected workflow:", line):
         # print(line.split("Injected workflow: ")[1], end="")
-        tt = (line.split("Injected workflow: ")[1])
-        print("ffsd", tt)
-        if tt == 'rverma_PR_reference_RelVal_306154_171209_114545_4322':
-            print("yes da")
         arr.append(line.split("Injected workflow: ")[1])
 
-# Sort out workflow names
+#Sort out workflow names
 arr2 = []
 n = int(len(arr) / 2)
 for x in range(0, n):
     arr2.append(arr[x])
     arr2.append(arr[n + x])
 
-ind = 0
-print()
-print (k[ind])
-
-mycount = 0
-# for x in range(0, len(k)):
-#     print(k[x])
+#Write to file
 for i in range(0, len(arr2)):
     print(arr2[i], end="")
-    mycount = mycount + 1
-    if(mycount % 3 == 2):
-        ind = ind + 1
-        mycount = mycount + 1
-        if(ind < len(k)):
-            print()
-            print (k[ind])
-
-print(len(arr))
+    f_out.write(arr2[i])
 
 
+#close all files
 f.close()
+f_out.close()
